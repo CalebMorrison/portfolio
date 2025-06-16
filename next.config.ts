61cc32878ae342d4d80b3b46d0e53be673435cdb
Caleb <calebmorr19@gmail.com>
2025-06-16 12:50:10 -0300

    troubleshooting

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // Optional: set these if deploying to a subpath like username.github.io/repo-name
-  // basePath: "/your-repo-name",
-  // assetPrefix: "/your-repo-name",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
 };
 
 export default nextConfig;
