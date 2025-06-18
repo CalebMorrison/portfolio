2bd2bf9c951f9708303fe13edcbf895d10d33d39
Caleb <calebmorr19@gmail.com>
2025-06-18 13:46:59 -0300

    setting up static page for gh-pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import HeroSection from './components/HeroSection';
-import SystemArchitecture from './components/PersonalProjects';
-import TechnicalMetrics from './components/WorkProjectsAchievements';
+import PersonalProjects from './components/PersonalProjects';
+import WorkProjectsAchievements from './components/WorkProjectsAchievements';
 import ContactSection from './components/ContactSection';
 
 export default function softwareDeveloperPortfolio() {
 	return (
+		//re-add personalProgects when something completed <PersonalProjects />
 		<main className="min-h-screen bg-[#1A1A1A] text-white overflow-x-hidden">
 			<HeroSection />
-			<SystemArchitecture />
-			<TechnicalMetrics />
+			<WorkProjectsAchievements />
 			<ContactSection />
 		</main>
 	);
